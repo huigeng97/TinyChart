497de1c34721277d9dc3ceaafe0ce73c833413e2
sudhirgahan <79176936+sudhirgahan@users.noreply.github.com>
2021-02-26 23:04:15 -0600

    Add files via upload
    Updated version that throws an exception if category is not found in csv file

--- MovieDataReader.java
+++ MovieDataReader.java
@@ -13,38 +13,42 @@ public class MovieDataReader implements MovieDataReaderInterface {
       throws FileNotFoundException, IOException, DataFormatException {
     List<MovieInterface> movies = new ArrayList<MovieInterface>();
     int count = 0;
-    int titleInd = 0, yearInd = 0, directorInd = 0, descriptInd = 0, genresInd = 0, voteInd = 0;
+    int titleInd = -1, yearInd = -1, directorInd = -1, descriptInd = -1, genresInd = -1,
+        voteInd = -1;
     String line = "";
-
-      BufferedReader br = new BufferedReader(inputFileReader);
-      while ((line = br.readLine()) != null) {
-        if (count == 0) {
-          String[] values = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
-          for (int i = 0; i < values.length; i++) {
-            if (values[i].equals("title")) {
-              titleInd = i;
-            } else if (values[i].contains("year")) {
-              yearInd = i;
-            } else if (values[i].contains("director")) {
-              directorInd = i;
-            } else if (values[i].contains("description")) {
-              descriptInd = i;
-            } else if (values[i].contains("vote")) {
-              voteInd = i;
-            } else if (values[i].contains("genre")) {
-              genresInd = i;
-            }
+    BufferedReader br = new BufferedReader(inputFileReader);
+    while ((line = br.readLine()) != null) {
+      if (count == 0) {
+        String[] values = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+        for (int i = 0; i < values.length; i++) {
+          if (values[i].equals("title")) {
+            titleInd = i;
+          } else if (values[i].contains("year")) {
+            yearInd = i;
+          } else if (values[i].contains("director")) {
+            directorInd = i;
+          } else if (values[i].contains("description")) {
+            descriptInd = i;
+          } else if (values[i].contains("vote")) {
+            voteInd = i;
+          } else if (values[i].contains("genre")) {
+            genresInd = i;
           }
         }
-        if (count > 0) {
-          String[] values = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
-          if (values.length != 13)
-            throw new DataFormatException();
-          movies.add(new Movie(values[titleInd], values[yearInd], values[directorInd],
-              values[descriptInd], values[genresInd], values[voteInd]));
+        if (titleInd == -1 || yearInd == -1 || directorInd == -1 || genresInd == -1
+            || voteInd == -1) {
+          throw new DataFormatException();
         }
-        count++;
       }
+      if (count > 0) {
+        String[] values = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+        if (values.length != 13)
+          throw new DataFormatException();
+        movies.add(new Movie(values[titleInd], values[yearInd], values[directorInd],
+            values[descriptInd], values[genresInd], values[voteInd]));
+      }
+      count++;
+    }
     return null;
   }
 
