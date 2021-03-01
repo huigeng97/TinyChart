b9d433f47c273179451330482413c77b017d75fb
Austin Cohen <69203996+acbm100@users.noreply.github.com>
2021-02-28 20:22:52 -0600

    Add files via upload

--- Frontend.java
+++ Frontend.java
@@ -9,14 +9,28 @@ import java.util.Scanner;
 public class Frontend {
 	private Scanner sc = new Scanner(System.in);
 	private Backend backend; //implementing backend code
+	private String [] selectedGenres;
+	private String [] selectedRatings;
+
+	public Frontend() {
+	}
 	/**
 	 * This method runs the base mode of the movie mapper frontend class
 	 * @param selectedGenres: an array of strings that say either SELECTED or UNSELECTED for each genre
 	 * @param selectedRatings: an array of strings that say either SELCETED or UNSELCTED for each rating
 	 * @param backend: the backend which is being used for this frontend interface
 	 */
-	public void runBaseMode(String[]selectedGenres, String[]selectedRatings, Backend backend) {
-		this.backend=backend;
+	public void runBaseMode() {
+		if (selectedGenres==null) {
+			selectedGenres=  new String [backend.getAllGenres().size()];
+			for (int i=0; i<backend.getAllGenres().size(); i++)
+				selectedGenres[i]="UNSELECTED";
+		}//beings selected & unselected genres 
+		if (selectedRatings==null) {
+			selectedRatings=  new String [11];
+			for (int i=0; i<11; i++)
+				selectedRatings[i]="UNSELECTED";
+		}//beings selected & unselected rankings 
 		String userInput="";
 		//welcome message
 		System.out.println("Welcome to MovieMapper Base Mode. You may type numbers to naviage through the "
@@ -24,14 +38,19 @@ public class Frontend {
 		System.out.println("Enter \"g\" to enter genre mode and enter \"r\" to enter ratings mode.");
 		//prints first three movies
 		//big for loop which keeps user in program "x" leaves
-		while (userInput.equals("x")) {
-			List<MovieInterface> currThree=backend.getThreeMovies(0);
-			//TODO: if I just print the titles or more info about the movie? 
-			for (int i=0; i<currThree.size();i++) {
-				System.out.print(currThree.get(i).getTitle()+" ");
-			}//prints first three movies 
-			System.out.println("Enter a number between 1 and "+(backend.getNumberOfMovies())+" to scroll through movies, "
-					+ "\"x\" to exit, \"g\" to enter genre mode, or \"r\" to enter ratings mode.");
+		List<MovieInterface> currThree;
+		int location=0;
+		while (!userInput.equals("x")) {
+			currThree=backend.getThreeMovies(location);
+			for (int i=0; i<currThree.size();i++)
+				System.out.println(currThree.get(i));
+			if (backend.getNumberOfMovies()==0)
+				System.out.println("There are no movies selected, \"x\" to exit, \"g\" to enter genre mode, or \"r\" to enter ratings mode.");
+			else if (backend.getNumberOfMovies()==1)
+				System.out.println("There is only one available movie, \"x\" to exit, \"g\" to enter genre mode, or \"r\" to enter ratings mode.");
+			else 
+				System.out.println("Enter a number between 1 and "+(backend.getNumberOfMovies())+" to scroll through movies, "
+						+ "\"x\" to exit, \"g\" to enter genre mode, or \"r\" to enter ratings mode.");
 			userInput=sc.next();
 			if (userInput.equals("x"))
 				break;//break loop if user enters "x"
@@ -40,7 +59,7 @@ public class Frontend {
 				continue;//return from genre mode
 			}
 			if (userInput.equals("r")) {//enters rating mode
-				runGenreMode(selectedRatings);
+				runRatingSelectionMode(selectedRatings);
 				continue;//returns from ratings mode
 			}
 			int x=turnStringToInt(userInput);//turns input into a string
@@ -48,17 +67,14 @@ public class Frontend {
 				System.out.println("Invalid Input");
 				continue;
 			}
-			currThree=backend.getThreeMovies(x-1);//moves down list of movies
-			for (int i=0; i<currThree.size();i++) {
-				System.out.println(currThree.get(i).getTitle());
-			}
+			location=x-1;//moves down list of movies
 		}
 	}
-/**
- * This method runs the genre mode which allows users to select and unselect genres to filter their movies
- * @param givenArr: an array of strings which says either "SELECTED" or "UNSELECTED" for each genre
- * @return: an array of strings which is the changed version of the entered array of "SELECTED"s and "UNSELECTED"s
- */
+	/**
+	 * This method runs the genre mode which allows users to select and unselect genres to filter their movies
+	 * @param givenArr: an array of strings which says either "SELECTED" or "UNSELECTED" for each genre
+	 * @return: an array of strings which is the changed version of the entered array of "SELECTED"s and "UNSELECTED"s
+	 */
 	public String [] runGenreMode(String []givenArr) {
 		List<String>genreList=backend.getAllGenres();
 		String [] arr=givenArr;
@@ -85,12 +101,12 @@ public class Frontend {
 				continue;
 			}
 			if(firstLetter=='s') {//selects genre
-				arr[genre]="SELECTED";
-				backend.addGenre(genreList.get(genre));
+				arr[genre-1]="SELECTED";
+				backend.addGenre(genreList.get(genre-1));
 			}
 			if(firstLetter=='u') {//unselects genre
-				arr[genre]="UNSELECTED";
-				backend.removeGenre(genreList.get(genre));
+				arr[genre-1]="UNSELECTED";
+				backend.removeGenre(genreList.get(genre-1));
 			}
 		}
 		return null;//should never get here
@@ -119,7 +135,7 @@ public class Frontend {
 				return arr;
 			char firstLetter=userInput.charAt(0);//breaks into s or d 
 			int rating=turnStringToInt(userInput.substring(1));//breaks into what specific rating
-			if ((firstLetter!='s'&&firstLetter!='u')||rating<1||rating>10) {//check input validity
+			if ((firstLetter!='s'&&firstLetter!='u')||rating<0||rating>10) {//check input validity
 				System.out.println("Invalid Input");
 				continue;
 			}
@@ -134,39 +150,52 @@ public class Frontend {
 		}
 		return null;
 	}
-/**
- * Helper method for all three modes which turns string input into a number
- * @param x: is the entered string
- * @return int: the number version of the entered string. If the string is not a number return -1
- */
+	/**
+	 * This method adds the backend to the frontend
+	 * @param backend: the specific backend to be added to the frontend
+	 */
+	public void run (Backend backend) {
+		if (backend==null)
+			return;
+		this.backend=backend;
+		runBaseMode();
+		return;
+	}
+
+	/**
+	 * Helper method for all three modes which turns string input into a number
+	 * @param x: is the entered string
+	 * @return int: the number version of the entered string. If the string is not a number return -1
+	 */
 	private int turnStringToInt(String x) {
 		int num=0;
 		for (int i=0;i<x.length(); i++) {
-			if ((int)x.charAt(i)>47&&(int)x.charAt(i)<58) 
-				num+=(int)x.charAt(i)*10^(x.length()-i);
+			if ((int)x.charAt(i)>47&&(int)x.charAt(i)<58) {
+				num+=(int)(x.charAt(i)-48)* Math.pow(10, x.length()-i-1);
+			}
 			else {
 				return -1;
 			}
 		}
 		return num;
 	}
-/**
- * This method prints all genres and whether they are selected or not
- * @param selected: an array of strings and whether a specific genre is selected or not
- * @param genres: the list of all genres
- */
+	/**
+	 * This method prints all genres and whether they are selected or not
+	 * @param selected: an array of strings and whether a specific genre is selected or not
+	 * @param genres: the list of all genres
+	 */
 	private void printGenres(String[]selected, List<String>genres ) {
 		for (int i=0; i<genres.size();i++) {
 			System.out.println((i+1)+". " +genres.get(i)+": "+selected[i]);
 		}
 	}
-/**
- * This method prints all ratings and states whether or not it is selected or not
- * @param selected: an array of whether or not a value is selected 
- */
+	/**
+	 * This method prints all ratings and states whether or not it is selected or not
+	 * @param selected: an array of whether or not a value is selected 
+	 */
 	private void printRatings(String []selected) {
-		for (int i=0; i<10; i++) {
-			System.out.println((i+1)+". "+selected[i]);
+		for (int i=0; i<11; i++) {
+			System.out.println((i)+". "+selected[i]);
 		}
 	}
 }
