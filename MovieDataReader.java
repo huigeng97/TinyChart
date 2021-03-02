254f795c1ef3293449856259f3a2c1853e4aacf5
sudhirgahan <79176936+sudhirgahan@users.noreply.github.com>
2021-03-02 12:55:26 -0600

    Add files via upload
    Commented code for all classes

--- MovieDataReader.java
+++ MovieDataReader.java
@@ -1,3 +1,13 @@
+// --== CS400 File Header Information ==--
+// Name: Gahan Sudhir
+// Email: gsudhir@wisc.edu
+// Team: GE red
+// Role: Data Wrangler
+// TA: Surabhi
+// Lecturer: Gary
+// Notes to Grader: Changed FileReader inputFileReader to Reader inputFileReader in order to
+// accommodate for StringReader in test cases
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -10,6 +20,11 @@ import java.util.zip.DataFormatException;
 public class MovieDataReader implements MovieDataReaderInterface {
 
   @Override
+  /**
+   * This method returns a list of movie objects
+   * 
+   * @return List<MovieInterface> of movies from parameter inputFileReader
+   */
   public List<MovieInterface> readDataSet(Reader inputFileReader)
       throws FileNotFoundException, IOException, DataFormatException {
     List<MovieInterface> movies = new ArrayList<MovieInterface>();
@@ -20,9 +35,10 @@ public class MovieDataReader implements MovieDataReaderInterface {
     BufferedReader br = new BufferedReader(inputFileReader);
     while ((line = br.readLine()) != null) {
       if (count == 0) {
-        String[] values = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+        String[] values = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1); // deals with quotes
+                                                                               // within quotes
         headerLength = values.length;
-        for (int i = 0; i < values.length; i++) {
+        for (int i = 0; i < values.length; i++) { // first loop establishes indexes to find
           if (values[i].equals("title")) {
             titleInd = i;
           } else if (values[i].contains("year")) {
@@ -43,26 +59,16 @@ public class MovieDataReader implements MovieDataReaderInterface {
         }
       }
       if (count > 0) {
-        String[] values = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+        String[] values = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1); // deals with quotes
+                                                                               // within quotes
         if (values.length != headerLength)
           throw new DataFormatException();
         movies.add(new Movie(values[titleInd], values[yearInd], values[directorInd],
             values[descriptInd], values[genresInd], values[voteInd]));
-        //System.out.println(movies.get(count-1));
       }
       count++;
     }
     return movies;
   }
-//
-//  public static void main(String[] args) {
-//    String path = "\\Users\\sudhi\\Downloads\\movies.csv";
-//    MovieDataReader mdr = new MovieDataReader();
-//    try {
-//      mdr.readDataSet(new FileReader(path));
-//    } catch (Exception e) {
-//      System.out.println("Error found!");
-//    }
-//  }
 
 }
